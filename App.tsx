6f55b3a14aef1cea996b7d38d4ff71b44bb4c479
JonathasLopes <jon.athas@hotmail.com>
2022-08-30 22:24:10 -0300

    splashScreen add to ios

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routes from './src/Routes';
+import SplashScreen from  "react-native-splash-screen";
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <Routes />
   );
